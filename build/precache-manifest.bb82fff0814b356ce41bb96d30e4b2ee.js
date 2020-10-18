0b328284ff4b2c04fc2cb2616ebfaba9393182f1
zhaodanchun <danchunzhao@tencent.com>
2020-10-18 19:53:48 +0800

    init

---
+++ build/precache-manifest.bb82fff0814b356ce41bb96d30e4b2ee.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5742baa1c174a766447c3e90df0a1854",
+    "url": "./index.html"
+  },
+  {
+    "revision": "f53b3521bc5bd7fbf686",
+    "url": "./static/css/main.5ee48e73.chunk.css"
+  },
+  {
+    "revision": "c805df33574d54f89f9d",
+    "url": "./static/js/2.7b1be5a8.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "./static/js/2.7b1be5a8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f53b3521bc5bd7fbf686",
+    "url": "./static/js/main.f1171d95.chunk.js"
+  },
+  {
+    "revision": "7beddeb4223c250b4079",
+    "url": "./static/js/runtime-main.da66a749.js"
+  }
+]);
